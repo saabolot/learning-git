9b77f3d8b1a8e37e157a2f2d7e38e70385f6ecba
Szymon Stencel <sstencel@gmail.com>
2022-01-02 23:47:17 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+liczby = list(range(0,101))  
+liczby_podzielne5=[]
+liczby3=[]
+for i in liczby:
+    if i % 5 == 0:
+        liczby_podzielne5.append(i)
+for i in liczby_podzielne5:
+    liczby3.append(i**3)
+print(liczby_podzielne5)
+print(liczby3)
